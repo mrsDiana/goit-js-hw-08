2251ad436a50ad488115d23ba2300af349a8d3c6
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-10-20 01:07:29 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import  throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(time, 60000))
+function time (time) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time.seconds))
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
