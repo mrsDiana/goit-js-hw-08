a7a3de0c2d31643bc286ff13defc479ee80a315b
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-10-23 20:38:43 +0300

    task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,29 @@
-import  throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(time, 60000))
-function time (time) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(time.seconds))
-};
+player.on('timeupdate', throttle(time, 1000));
+function time(time) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
+}
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
     // seconds = the actual time that the player seeked to
-}).catch(function(error) {
+  })
+  .catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-        default:
-            // some other error occurred
-            break;
+      default:
+        // some other error occurred
+        break;
     }
-});
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    inputTxt: document.querySelector('.feedback-form input')
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onTextareaInput);
+
+populateTextInput ();
+
+function onFormSubmit (e) {
+e.preventDefault();
+e.currentTarget.reset();
+localStorage.removeItem('feedback-form-state');
+}
+
+function onTextareaInput(e){
+    const {elements: {email, message}} = e.currentTarget;
+    const messageForm = {email: email.value , message: message.value};
+    localStorage.setItem('feedback-form-state', JSON.stringify(messageForm));
+}
+
+function populateTextInput (){
+    const saveMessage = localStorage.getItem('feedback-form-state');
+    
+    if (saveMessage) {
+    const saveMessageJSON = JSON.parse(saveMessage);
+    console.log(saveMessageJSON);
+    refs.textarea.value = saveMessageJSON.message;
+    refs.inputTxt.value = saveMessageJSON.email;
+    }
+}
